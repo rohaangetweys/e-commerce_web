e3c8ed269744239106e8f2030a669d8075a95d9b
rohaanumair <m.rohaanumair@gmail.com>
2025-11-19 13:26:07 +0500

    removed header from auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <Header isAuthPage />
-            <main className='w-full min-h-screen bg-[#EBEEF6] max-w-screen-2xl mx-auto overflow-hidden px-6 pt-[92px] flex flex-col justify-center items-center'>
+            {/* <Header isAuthPage /> */}
+            <main className='w-full min-h-screen bg-[#EBEEF6] max-w-screen-2xl mx-auto overflow-hidden px-6 flex flex-col justify-center items-center'>
                 {children}
             </main>
         </>
