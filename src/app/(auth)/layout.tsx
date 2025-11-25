1377891ccf57f8c4320f4250f4e5483456ea2d56
rohaanumair <m.rohaanumair@gmail.com>
2025-11-25 17:50:40 +0500

    auth ui fix for small screen

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <main className='w-full min-h-screen bg-[#EBEEF6] max-w-screen-2xl mx-auto overflow-hidden px-6 flex flex-col justify-center items-center'>
+            <main className='w-full min-h-screen bg-[#EBEEF6] max-w-screen-2xl mx-auto overflow-hidden px-6 flex flex-col justify-center items-center max-h-screen'>
                 {children}
             </main>
         </>
